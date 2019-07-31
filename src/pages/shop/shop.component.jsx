2a2da6bd4c8dc3f802c3eddbd0acaefd5ba077c9
Yeshwanth Kanneganti <yeshwanth.js@gmail.com>
2019-07-31 10:59:50 -0300

    added props in contructor

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import SHOP_DATA from './shop.data.js';
 import CollectionPreview from'../../components/preview-collection/preview-collection.component';
 class ShopPage extends React.Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
             collections: SHOP_DATA
         };
