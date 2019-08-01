de4216658903760d3f2af7c3a72b44254dad8134
Yeshwanth Kanneganti <yeshwanth.js@gmail.com>
2019-08-01 10:50:06 -0300

    added a component

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import './sign-in-and-sign-up.styles.scss'
+
+const signInAndSignUpPage = () => (
+    <div className='sign-in-and-sign-up'>SIGN IN</div>
+);
+export default signInAndSignUpPage;
